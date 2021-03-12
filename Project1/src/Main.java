2e5bf6dd05afb67273adb6e3dfabe9c7bd97f27c
Farshid <farshidnooshi726@aut.ac.ir>
2021-03-12 15:39:16 +0330

    .jar added

--- Project1/src/Main.java
+++ Project1/src/Main.java
@@ -1,5 +1,11 @@
+import redis.clients.jedis.Jedis;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Jedis jedis = new Jedis("localhost");
+        jedis.set("events/city/rome", "32,15,223,828");
+        String cachedResponse = jedis.get("events/city/rome");
+        System.out.println(cachedResponse);
     }
 }
